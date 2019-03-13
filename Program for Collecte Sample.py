9b656ad6e3dc0d9eb9f27e6a42a31c97925361ef
Dushyant Sankhla <46795000+dush4439@users.noreply.github.com>
2019-03-13 09:43:40 -0700

    Update Program for Collecte Sample.py

--- Program for Collecte Sample.py
+++ Program for Collecte Sample.py
@@ -1,24 +1,29 @@
-
 #Semple clection of user Face
-import cv2
-import numpy as np
+import cv2 # impoort openCV library
+import numpy as np #import nympy for nomerical method
 
-face_classifier = cv2.CascadeClassifier('C:/Users/dush4/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+face_classifier = cv2.CascadeClassifier('C:/Users/dush4/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')#location for CascadeClassifire pre defind face data
 
+#Defind a Function for extractor
 def face_extractor(img):
+    #convert RBG image in gray scale image
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
     if faces is():
         return None
     for(x,y,w,h) in faces:
+        # give the dimention for cropped face
         cropped_face = img[y:y+h, x:x+w]
         return cropped_face
 
-cap = cv2.VideoCapture(0)
-count = 0
+#on camera
+cap = cv2.VideoCapture(0) 
+#initialization of count variable
+count = 0     
 while True:
     ret, frame = cap.read()
-    if face_extractor(frame) is not None:
+    #If face is Detect Then execute the program
+    if face_extractor(frame) is not None: 
         count +=1
         face = cv2.resize(face_extractor(frame),(200,200))
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
